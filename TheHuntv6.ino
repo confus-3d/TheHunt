0a69c84fd39d223e9e63cc18cc9bf793c5a13902
Francisco <66030077+confus-3d@users.noreply.github.com>
2020-06-17 10:36:48 +0200

    Update TheHuntv6.ino

--- TheHuntv6.ino
+++ TheHuntv6.ino
@@ -494,7 +494,7 @@ void keyDisplayLoop() { //Show KEY status
   }
 }
 void monsterDisplayLoop() { //Fade MONSTER position while all board is OFF
-
+    setColor(OFF); 
     if (Hear == NOISE) {
       if (HeartBeat.isExpired()) { 
         if ( (brightness + step > MAX_BRIGHTNESS ) || (brightness + step < 0 ) ) {
@@ -536,7 +536,7 @@ void monsterDisplayLoop() { //Fade MONSTER position while all board is OFF
 }
 
 void citizenDisplayLoop() {//Fade CITIZEN position while all board is OFF
-    
+    setColor(OFF); 
     if (Smell == STINK) {
       if (HeartBeat.isExpired()) { 
         if ( (brightness + step > MAX_BRIGHTNESS ) || (brightness + step < 0 ) ) {
